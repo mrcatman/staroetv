8ced6bff45eaddf6c6e69cae0c54defe50588c61
Kirill Alexeev-Kotov <k_alexeev-kotov@gcore.lu>
2026-02-12 14:42:47 +0300

    загрузка записей с yt

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::any('api/search', [\App\Http\Controllers\RecordsController::class, 'apiSearch']);
-Route::post('api/records/on-downloaded/{id}', [\App\Http\Controllers\RecordsUploadController::class, 'onDownloaded']);
+Route::any('search', [\App\Http\Controllers\RecordsController::class, 'apiSearch']);
+Route::post('records/on-downloaded/{id}', [\App\Http\Controllers\RecordsUploadController::class, 'onDownloaded']);
