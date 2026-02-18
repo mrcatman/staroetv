48624c53f2b054dd60f3af3b0b270e2105f05d1b
Kirill Alexeev-Kotov <k_alexeev-kotov@gcore.lu>
2026-02-18 16:19:42 +0300

    обновление обложек телетекста

---
+++ app/Console/Commands/MakeTeletextThumbnails.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Helpers\TeletextHelper;
+use App\Models\Teletext;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class MakeTeletextThumbnails extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'teletext:thumbnails';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import teletext from remote URL';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $teletexts = Teletext::whereNull('cover_id')->get();
+        foreach ($teletexts as $teletext) {
+            TeletextHelper::takeScreenshot($teletext);
+            Log::info('Teletext '.$teletext->id.': screenshot ready');
+        }
+    }
+}
