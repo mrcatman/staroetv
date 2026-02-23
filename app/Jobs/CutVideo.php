1c2a92f87d67e32592cb32e5868cffe334afd54b
Kirill Alexeev-Kotov <k_alexeev-kotov@gcore.lu>
2026-02-24 02:30:09 +0300

    конвертация во временную папку

--- app/Jobs/CutVideo.php
+++ app/Jobs/CutVideo.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use App\Helpers\MediaHelper;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Storage;
 
 class CutVideo implements ShouldQueue
@@ -21,7 +22,9 @@ class CutVideo implements ShouldQueue
     public function handle(): void
     {
         $storage = Storage::disk('media-storage');
+        $temp_path = public_path("temp_videos/{$this->filename}.mp4");
         $output_path = $storage->path("videos/{$this->filename}.mp4");
-        MediaHelper::cutVideo($this->path, $this->start, $this->end, $output_path);
+        MediaHelper::cutVideo($this->path, $this->start, $this->end, $temp_path);
+        Process::forever()->run("mv $temp_path $output_path");
     }
 }
