c47bcb10e15fb130fd8936b33a74b243be4088d2
Kirill Alexeev-Kotov <k_alexeev-kotov@gcore.lu>
2026-05-01 16:17:22 +0300

    фикс работы массового загрузчика

--- resources/js/components/mass-uploader/MassUploaderRecordForm.vue
+++ resources/js/components/mass-uploader/MassUploaderRecordForm.vue
@@ -398,8 +398,7 @@ parseTitle();
 if (props.record.file) {
     data.value.uploaded_file_url = props.record.file;
 } else {
-    const file = props.files.find(file => file.replace('.mp4', '') === data.title);
-    console.log(file);
+    const file = props.files.find(file => file.replace('.mp4', '') === data.value.title);
     if (file) {
         data.value.uploaded_file_url = `${file}.mp4`;
     }
