a5d0f95c1f7f47141452cf8b1ae4cfbf5c8a530b
Kirill Alexeev-Kotov <k_alexeev-kotov@gcore.lu>
2026-06-29 23:33:40 +0300

    дебаг

--- resources/js/promo/src/playback.ts
+++ resources/js/promo/src/playback.ts
@@ -126,10 +126,13 @@ export const Playback = {
             this.player.play();
             setTimeout(() => {
                 try {
-                    this.player.setVolume(parseFloat(localStorage.getItem('volume')) ?? 1);
+                    this.player.setVolume(1);
+                    alert('volume set');
+                    //this.player.setVolume(parseFloat(localStorage.getItem('volume')) ?? 1);
                 } catch (e) {
+                    alert(e.toString());
                 }
-            }, 200);
+            }, 500);
 
             this.recordTimeout = setTimeout(() => {
                 console.log('started', started);
