ca94f57dff40ba078ecd244fc36fc645ac3b8d7f
Kirill Alexeev-Kotov <k_alexeev-kotov@gcore.lu>
2026-06-30 00:14:24 +0300

    unmute плеера вк

--- resources/js/promo/src/players.ts
+++ resources/js/promo/src/players.ts
@@ -7,7 +7,7 @@ class VKPlayer extends EventEmitter implements Promo.Player  {
 
     load(url: string, container: HTMLDivElement)  {
         return new Promise<void>((resolve, reject) => {
-            container.innerHTML = `<iframe src="${url}&js_api=1&autoplay=1" frameborder="0" allowfullscreen allow="autoplay"></iframe>`;
+            container.innerHTML = `<iframe src="${url}&js_api=1" frameborder="0" allowfullscreen></iframe>`;
 
             const iframe = container.querySelector('iframe');
             this.instance = VK.VideoPlayer(iframe);
