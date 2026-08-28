6bdef3032f2d24b1ba2143e2a3e7e6cb1b1ce0f2
Kirill Alexeev-Kotov <k_alexeev-kotov@gcore.lu>
2026-08-29 02:48:19 +0300

    конвертация в webm - доп. проверка на существование видео, прогрессбар

--- app/Console/Commands/EnableWebmForConvertedVideos.php
+++ app/Console/Commands/EnableWebmForConvertedVideos.php
@@ -28,7 +28,14 @@ class EnableWebmForConvertedVideos extends Command
     public function handle()
     {
         $storage = Storage::disk('media-storage');
-        foreach ($storage->allFiles('videos') as $file) {
+        $files = $storage->allFiles('videos');
+
+        $progressBar = $this->output->createProgressBar(count($files));
+        $progressBar->start();
+
+        foreach ($files as $file) {
+            $progressBar->advance();
+
             if (!str_ends_with($file, '.mp4')) {
                 continue;
             }
@@ -41,10 +48,16 @@ class EnableWebmForConvertedVideos extends Command
 
             $last_modified = $storage->lastModified($webm_path);
             if (time() - $last_modified < 60) { // в процессе конвертации
+                echo "Converting now: {$file}\n";
                 continue;
             }
 
             $video = Record::where('source_path', 'LIKE', "%{$file}%")->first();
+            if (!$video) {
+                echo "Not found: {$file}\n";
+                continue;
+            }
+
             echo "{$video->id}: {$video->title}\n";
 
             if ($this->option('confirm')) {
@@ -53,5 +66,7 @@ class EnableWebmForConvertedVideos extends Command
                 $video->save();
             }
         }
+        
+        $progressBar->finish();
     }
 }
