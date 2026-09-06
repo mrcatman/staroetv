fccef340fcd0e958c385513abb2e5c1357c6b2fe
Kirill Alexeev-Kotov <k_alexeev-kotov@gcore.lu>
2026-09-06 18:57:46 +0300

    фикс определения сафари

--- resources/js/promo/utils.ts
+++ resources/js/promo/utils.ts
@@ -40,5 +40,8 @@ export const getFullPictureUrl = (url: string) => {
 }
 
 export const isSafari = () => {
-    return !CSS.supports('height', '100dvh') || !('showOpenFilePicker' in window);
+    return navigator.vendor.match(/apple/i) &&
+        !navigator.userAgent.match(/crios/i) &&
+        !navigator.userAgent.match(/fxios/i) &&
+        !navigator.userAgent.match(/Opera|OPT\//);
 }
